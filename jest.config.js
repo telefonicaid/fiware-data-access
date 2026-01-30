a75ebb5786331e2f14012826cf4c88b7c5c882df
Alvaro Vega <alvaro.vegagarcia@telefonica.com>
2026-01-30 13:01:35 +0100

    add coverage

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ export default {
   testEnvironment: 'node',
   testMatch: ['**/*.test.js'],
   verbose: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
 };
