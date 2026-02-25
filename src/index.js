22620c4829209e5e5d74002adb617015b3bb50b1
GregorioBlazquez <gregorio.blazquez@estudiante.uam.es>
2026-02-25 10:25:30 +0100

    Fix error

--- src/index.js
+++ src/index.js
@@ -176,7 +176,7 @@ app.put('/fdas/:fdaId', async (req, res) => {
     });
   }
 
-  if (Object.keys(req.body).length > 0) {
+  if (req.body && Object.keys(req.body).length > 0) {
     return res.status(400).json({
       error: 'BadRequest',
       description: 'PUT /fdas does not accept a request body',
