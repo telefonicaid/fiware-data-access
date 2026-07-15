08b3079e7904e42aa4836f9f7c43478b6c6ece86
GregorioBlazquez <gregorio.blazquezmartinez@telefonica.com>
2026-07-15 10:35:39 +0200

    Fix Issue sonarCube

--- src/lib/utils/db.js
+++ src/lib/utils/db.js
@@ -140,7 +140,7 @@ function buildStoredSchemaQuery(schema, userQuery) {
   const selectList = schemaFields
     .map(
       ({ name, type }) =>
-        `CAST(NULL AS ${type}) AS "${String(name).replaceAll(/"/g, '""')}"`,
+        `CAST(NULL AS ${type}) AS "${String(name).replaceAll('"', '""')}"`,
     )
     .join(', ');
 
